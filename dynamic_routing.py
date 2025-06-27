cfa2735f3d551a7d5f27a4365e910e4dca2691b2
Maithili-Badhan <maithilibprojects@gmail.com>
2025-06-27 21:50:39 +0530

    dynamic routing

---
+++ dynamic_routing.py
@@ -0,0 +1,14 @@
+from flask import Flask, escape
+
+import requests
+
+app = Flask(__name__)
+
+@app.route("/book/<isbn>")
+def get_author(isbn):
+    res = requests.get("https://openlibrary.org/isbn/{escape(isbn)}.JSON")
+
+    if res.status_code == 200:
+        return {"message": res.JSON()}
+    elif res.status_code == 404:
+        return {"message": "Something went wrong!"}, 404
