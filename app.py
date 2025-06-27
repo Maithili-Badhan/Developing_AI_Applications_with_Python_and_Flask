e1ad3c859c2eb7e33dd13959d50e201af4f9f8c9
Maithili-Badhan <maithilibprojects@gmail.com>
2025-06-27 19:34:39 +0530

    Returning JSON method 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return {"message": "Hello World"}
+    return jsonify(message="Hello World")
